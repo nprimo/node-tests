0d195c638794e2f10aeca9f1051d67a5e0ef7047
nprimo <primo.niccolo@gmail.com>
2022-10-20 17:52:06 +0100

    stable version for server testing
    - client can start server and log errors when failing to start
    - client can send requests and return values to be tested

--- client.js
+++ client.js
@@ -1,30 +1,30 @@
+import { once } from "events";
 import * as cp from "node:child_process";
-import { promisify } from "node:util";
 
-// Start server
-
+const server = cp.spawn("node", ["server.js"]);
 const port = 5000;
 
+const message = await Promise.race([
+  once(server.stdout, "data"),
+  Promise.race([
+    once(server.stderr, "data").then(String).then(Error),
+    once(server, "error"),
+  ]).then((x) => Promise.reject(x)),
+]);
+
+// check the 1st log: for example if they specified the PORT
+message[0].toString("utf8");
+
 const sendRequest = async (path, options) => {
-  try {
-    const response = await fetch(`http://localhost:${port}${path}`, options);
-    console.log("--- Check status code:");
-    console.log(response.status);
-    console.log(response.statusText);
-    if (response.ok) {
-      console.log("--- Response:");
-      try {
-        console.log(await response.json());
-      } catch (err) {
-        console.log("Need to check server ...");
-        console.log(err);
-      }
-    }
-  } catch (err) {
-    console.log("--- Something bad happened");
-    console.log(err);
-  }
+  const response = await fetch(`http://localhost:${port}${path}`, options);
+  // return an object
+  const { status, statusText, ok } = response;
+  const headers = Object.fromEntries(response.headers);
+  const body = await response.text();
+  return { status, body, headers };
 };
+
+// tests here
 
 await sendRequest("/db/mario12.json", {
   method: "GET",
@@ -37,4 +37,4 @@ await sendRequest("/db/mario1.json", {
   body: "123",
 });
 
-// Shutdown server
+server.kill("SIGQUIT");
